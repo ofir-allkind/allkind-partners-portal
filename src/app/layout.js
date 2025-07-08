b9317d02ba57e33bc90c1e811559b38fad1e806c
Ofir Bar <ofir@allkind.inc>
2025-07-08 17:53:53 +0300

    Fixed broken favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,6 +16,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" type="image/png" />
+      </head>
       <body className={inter.className}>
         <SidebarProvider>
           <div className="flex flex-col min-h-screen">
